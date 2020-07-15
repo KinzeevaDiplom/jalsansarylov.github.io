eac93a79ef0503a8ef3eb5078f434ff8d95f83f0
SarylovJalsan <jalsan.github@yandex.ru>
2020-07-15 19:06:26 +0300

    add responsive

--- landingPage1/js/main.js
+++ landingPage1/js/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
-  $(".header-slider").slick();
+  $(".header-slider").slick({
+    arrows: true,
+    // убирает стрелки при разрешении 768px
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
   $(".news__slider").slick();
-  $(".slick-prev").text("<");
-  $(".slick-next").text(">");
+  // для смены текста стрелок
+  setInterval(() => {
+    $(".slick-prev").text("<");
+    $(".slick-next").text(">");
+  }, 100);
+
+  // кнопка меню на мобилках
+  $(".gmr-btn").click(function () {
+    $(".gmr-btn").toggleClass("gmr-btn__active");
+    $(".gmr-menu").toggleClass("gmr-menu__active");
+  });
 });
