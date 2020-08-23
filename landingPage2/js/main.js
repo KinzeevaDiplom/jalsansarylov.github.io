dcfe1fd223bd4531dc73c65b2702396339735f7e
SarylovJalsan <jalsan.github@yandex.ru>
2020-08-23 12:51:46 +0300

    add surf slider

--- landingPage2/js/main.js
+++ landingPage2/js/main.js
@@ -19,4 +19,12 @@ $(document).ready(function () {
   $(".slider-dots .slick-slide").on("click", function (event) {
     $(".header__slider").slick("slickGoTo", $(this).data("slickIndex"));
   });
+  $(".surf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class = "slider-arrows slider-arrows__left" src="img/left-arrow-white.svg" alt="">',
+    nextArrow:
+      '<img class = "slider-arrows slider-arrows__right" src="img/header-array.svg" alt="array" />',
+  });
 });
