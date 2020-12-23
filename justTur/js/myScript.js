42253aab0dc22ff0cd79d61a14aa076d7d5aac8c
Jalsan Sarylov <jalsan.github@yandex.ru>
2020-12-23 20:06:06 +0300

    add justTur

---
+++ justTur/js/myScript.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.fire-tur__slider').slick({
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.feedback-slider').slick({
+        dots:true,
+    });
+
+})
